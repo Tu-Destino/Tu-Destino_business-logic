5dea51ae4778d116ea47bf288187cbe48ec3535a
patoDuraznoGalactico <155923871+patoDuraznoGalactico@users.noreply.github.com>
2024-05-05 16:16:18 -0500

    Feat: create folder structure

---
+++ Tu_Destino/src/main/java/com/riwi/Tu_Destino/TuDestinoApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.Tu_Destino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuDestinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuDestinoApplication.class, args);
+	}
+
+}
