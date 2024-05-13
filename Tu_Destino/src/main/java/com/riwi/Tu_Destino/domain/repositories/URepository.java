34114fa8060aef64d50106e990fad30679e8b1e9
Kristianmx <rivaskristopher24@gmail.com>
2024-05-13 11:58:23 -0500

    Refector: reorganization of surplus classes and folders

--- Tu_Destino/src/main/java/com/riwi/Tu_Destino/domain/repositories/URepository.java
+++
@@ -1,7 +0,0 @@
-package com.riwi.Tu_Destino.domain.repositories;
-
-import com.riwi.Tu_Destino.domain.entities.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface URepository extends JpaRepository<Usuario,String> {
-}
